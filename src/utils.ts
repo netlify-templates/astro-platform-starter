c8ae03332d97f75aebb2fba4dcefb097b8714615
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-04-18 19:56:34 +0300

    Revalidation by tag example

--- src/utils.ts
+++ src/utils.ts
@@ -41,3 +41,15 @@ export function generateBlob(parameters?: any) {
     const { path: svgPath, seedValue: seed } = blobshape(parameters);
     return { parameters: { ...parameters, seed }, svgPath };
 }
+
+export function cacheHeaders(maxAgeDays = 365, cacheTags?: string[]): Record<string, string> {
+    // As far as the browser is concerned, it must revalidate on every request.
+    // However, Netlify CDN is told to keep the content cached for up to maxAgeDays (note: new deployment bust the cache by default).
+    // We're also setting cache tags to be able to later purge via API (see: https://www.netlify.com/blog/cache-tags-and-purge-api-on-netlify/)
+    const headers = {
+        'Cache-Control': 'public, max-age=0, must-revalidate', // Tell browsers to always revalidate
+        'Netlify-CDN-Cache-Control': `public, max-age=${maxAgeDays * 86_400}, must-revalidate` // Tells Netlify CDN the max allwed cache duration
+    };
+    if (cacheTags?.length > 0) headers['Cache-Tag'] = cacheTags.join(',');
+    return headers;
+}

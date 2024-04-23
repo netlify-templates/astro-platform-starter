e8bc1b09e5229fa4e01baad58aae4f18b35e83ab
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-04-23 17:48:22 +0300

    Inline noise.png

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
+const fs = require('fs');
+
+const noiseBitmap = fs.readFileSync('./public/images/noise.png', { encoding: 'base64' });
+const noiseDataUri = 'data:image/png;base64,' + noiseBitmap;
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
             backgroundImage: {
-                'grid-pattern': "linear-gradient(to bottom, theme('colors.neutral.950 / 0%'), theme('colors.neutral.950 / 100%')), url('/images/noise.png')"
+                'grid-pattern': `linear-gradient(to bottom, theme('colors.neutral.950 / 0%'), theme('colors.neutral.950 / 100%')), url('${noiseDataUri}')`
             },
             colors: {
                 neutral: colors.neutral

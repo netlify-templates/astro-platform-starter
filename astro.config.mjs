f7314ad084499bb1b8739f2a58eaa8dc2c570678
Sean C Davis <scdavis41@gmail.com>
2024-09-26 06:22:00 -0400

    Reset layout and style header for compose

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), icon()],
   output: 'hybrid',
   adapter: netlify(),
 });

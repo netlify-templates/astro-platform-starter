6f3251e606387bcf8d366a2cf89da968eb855e7b
Sean C Davis <scdavis41@gmail.com>
2024-09-27 06:48:20 -0400

    Introduce page collection a layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind(), icon(), mdx()],
   output: 'hybrid',
   adapter: netlify(),
 });

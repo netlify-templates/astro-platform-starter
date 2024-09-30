86cb503ee16e9da7e566e31ed42bf3cfc8f9f6c7
Sean C Davis <scdavis41@gmail.com>
2024-09-30 05:40:08 -0400

    Add and configure expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,34 @@ import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
 
+import expressiveCode from 'astro-expressive-code';
+
 export default defineConfig({
-  integrations: [react(), tailwind(), icon(), mdx()],
+  integrations: [
+    react(),
+    tailwind(),
+    icon(),
+    expressiveCode({
+      themes: 'poimandres',
+      styleOverrides: {
+        borderColor: 'transparent',
+        borderRadius: '5px',
+        codeFontSize: '0.9rem',
+        codePaddingBlock: '2em',
+        codePaddingInline: '2em',
+        textMarkers: {
+          backgroundOpacity: '30%',
+          insHue: '175',
+          delHue: '15',
+        },
+        frames: {
+          tooltipSuccessBackground: '#5de4c7',
+          tooltipSuccessForeground: '#12181f',
+        },
+      },
+    }),
+    mdx(),
+  ],
   output: 'hybrid',
   adapter: netlify(),
 });

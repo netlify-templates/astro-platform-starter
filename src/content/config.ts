792b213500bd1f781dd150f44cb529fd2a7d9ae4
Sean C Davis <scdavis41@gmail.com>
2024-09-30 06:32:13 -0400

    Rework the revalidation page

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const pageCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
+    subheading: z.string().optional(),
   }),
 });
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import blobshape from 'blobshape';
-import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
+import { adjectives, animals, uniqueNamesGenerator } from 'unique-names-generator';
 
 // Note: this only works on the server side
 export function getNetlifyContext() {
@@ -44,13 +44,19 @@ export function generateBlob(parameters?: any) {
 
 export function cacheHeaders(maxAgeDays = 365, cacheTags?: string[]): Record<string, string> {
   // As far as the browser is concerned, it must revalidate on every request.
-  // However, Netlify CDN is told to keep the content cached for up to maxAgeDays (note: new deployment bust the cache by default).
-  // We're also setting cache tags to be able to later purge via API (see: https://www.netlify.com/blog/cache-tags-and-purge-api-on-netlify/)
-  const headers = {
-    'Cache-Control': 'public, max-age=0, must-revalidate', // Tell browsers to always revalidate
-    'Netlify-CDN-Cache-Control': `public, max-age=${maxAgeDays * 86_400}, must-revalidate`, // Tells Netlify CDN the max allwed cache duration
+  // However, Netlify CDN is told to keep the content cached for up to
+  // maxAgeDays (note: new deployment bust the cache by default). We're also
+  // setting cache tags to be able to later purge via API (see:
+  // https://www.netlify.com/blog/cache-tags-and-purge-api-on-netlify/)
+  const headers: Record<string, string> = {
+    // Tell browsers to always revalidate
+    'Cache-Control': 'public, max-age=0, must-revalidate',
+    // Tell Netlify CDN the max allowed cache duration
+    'Netlify-CDN-Cache-Control': `public, max-age=${maxAgeDays * 86_400}, must-revalidate`,
   };
-  if (cacheTags?.length > 0) headers['Cache-Tag'] = cacheTags.join(',');
+  if ((cacheTags || []).length > 0) {
+    headers['Cache-Tag'] = (cacheTags || []).join(',');
+  }
   return headers;
 }
 

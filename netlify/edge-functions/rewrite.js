fd87f3951a6c5028fa4fde2e84301bb41d1665db
TomasBankauskas <tbdizainas@gmail.com>
2024-10-01 19:03:09 +0300

    updated npm dependencies and edge-function

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,10 +1,8 @@
-const rewrite = async (request, context) => {
+export default async (request, context) => {
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
-    return new URL(path, request.url);
+    return Response.redirect(new URL(path, request.url));
 };
 
 export const config = {
     path: '/edge'
 };
-
-export default rewrite;

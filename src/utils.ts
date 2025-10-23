aa266ce9b850634591643936a02aca8592975f2d
Elad Rosenheim <eladroz@users.noreply.github.com>
2025-10-23 16:17:55 +0300

    Bump version; netlify adapter always available (#338)

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,6 @@
 import blobshape from 'blobshape';
 import { uniqueNamesGenerator, adjectives, animals } from 'unique-names-generator';
 
-// Note: this only works on the server side
-export function getNetlifyContext() {
-    return process.env.CONTEXT;
-}
-
 export function randomInt(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }

70db78d423f497b1d3f0a76e350230774e33a010
Elad Rosenheim <elad.rosenheim@gmail.com>
2025-10-23 16:07:54 +0300

    Bump version; netlify adapter always available

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,9 @@ export default defineConfig({
         plugins: [tailwindcss()]
     },
     integrations: [react()],
-    adapter: netlify()
+    adapter: netlify({
+        devFeatures: {
+            environmentVariables: true
+        }
+    })
 });
